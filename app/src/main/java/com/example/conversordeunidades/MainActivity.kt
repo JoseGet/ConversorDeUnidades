4494af549939247287e794f432d2f360f082ea57
Jose Getulio <getuliojose96@gmail.com>
2024-02-15 16:07:30 -0400

    App Conversor de Unidades

---
+++ app/src/main/java/com/example/conversordeunidades/MainActivity.kt
@@ -0,0 +1,213 @@
+package com.example.conversordeunidades
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.conversordeunidades.ui.theme.ConversorDeUnidadesTheme
+import kotlin.math.roundToInt
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ConversorDeUnidadesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ConversorDeUnidades()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ConversorDeUnidades()
+{
+
+    var ValorEntrada by remember { mutableStateOf("") }
+    var ValorSaida by remember { mutableStateOf("") }
+    var UnidadeEntrada by remember { mutableStateOf("Metros") }
+    var UnidadeSaida by remember { mutableStateOf("Metros") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val FatordeConversao = remember { mutableStateOf(1.00) }
+    val oFatordeConversao = remember { mutableStateOf(1.00) }
+
+
+    fun conversor()
+    {
+        // ?: Elvis Operator
+        val ValorEntradaDouble = ValorEntrada.toDoubleOrNull() ?: 0.0;
+        val resultado = (ValorEntradaDouble * FatordeConversao.value * 100.00/ oFatordeConversao.value).roundToInt() / 100.0
+        ValorSaida = resultado.toString();
+    }
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Conversor de Unidades",
+            style = MaterialTheme.typography.headlineLarge
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = ValorEntrada, onValueChange = {
+            ValorEntrada = it;
+            conversor();
+        }, label = { Text(text = "Digite um valor") })
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            // Caixa de Entrada
+            Box {
+                Button(onClick = { iExpanded = true }) {
+                    Text(text = UnidadeEntrada);
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Seta para baixo");
+                }
+
+                DropdownMenu(expanded = iExpanded, onDismissRequest = { iExpanded = false }) {
+
+                    DropdownMenuItem(text = { Text(text = "Centimetros") },
+                        onClick = {
+
+                            iExpanded = false
+                            UnidadeEntrada = "Centimetros"
+                            FatordeConversao.value = 0.01;
+                            conversor()
+                        }
+                    )
+
+                    DropdownMenuItem(text = { Text(text = "Metros") },
+                        onClick = {
+
+                            iExpanded = false
+                            UnidadeEntrada = "Metros"
+                            FatordeConversao.value = 1.00;
+                            conversor()
+                        }
+                    )
+
+                    DropdownMenuItem(text = { Text(text = "Pes") },
+                        onClick = {
+
+                            iExpanded = false
+                            UnidadeEntrada = "Pes"
+                            FatordeConversao.value = 0.3048;
+                            conversor()
+                        }
+                    )
+
+                    DropdownMenuItem(text = { Text(text = "Milimetros") },
+                        onClick = {
+
+                            iExpanded = false
+                            UnidadeEntrada = "Milimetros"
+                            FatordeConversao.value = 0.001;
+                            conversor()
+                        }
+                    )
+
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            // Caixa de Saida
+            Box {
+                Button(onClick = { oExpanded = true }) {
+                    Text(text = UnidadeSaida);
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Seta para baixo");
+                }
+
+                DropdownMenu(expanded = oExpanded, onDismissRequest = { oExpanded = false }) {
+
+                    DropdownMenuItem(text = { Text(text = "Centimetros") },
+                        onClick = {
+
+                            oExpanded = false
+                            UnidadeSaida = "Centimetros"
+                            oFatordeConversao.value = 0.01;
+                            conversor()
+                        }
+                    )
+
+                    DropdownMenuItem(text = { Text(text = "Metros") },
+                        onClick = {
+
+                            oExpanded = false
+                            UnidadeSaida = "Metros"
+                            oFatordeConversao.value = 1.00;
+                            conversor()
+                        }
+                    )
+
+                    DropdownMenuItem(text = { Text(text = "Pes") },
+                        onClick = {
+
+                            oExpanded = false
+                            UnidadeSaida = "Pes"
+                            oFatordeConversao.value = 0.3048;
+                            conversor()
+                        }
+                    )
+
+                    DropdownMenuItem(text = { Text(text = "Milimetros") },
+                        onClick = {
+
+                            oExpanded = false
+                            UnidadeSaida = "Milimetros"
+                            oFatordeConversao.value = 0.001;
+                            conversor()
+                        }
+                    )
+                }
+            }
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Resultado: $ValorSaida $UnidadeSaida",
+            style = MaterialTheme.typography.headlineMedium
+            )
+        
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConversorDeUnidadesPreview(){
+    ConversorDeUnidades();
+}
